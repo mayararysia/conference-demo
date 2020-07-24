21652a9de0acf8cdaa090c7767a67a4076faa53d
mayararysia <mayara.ryzia@gmail.com>
2020-07-24 11:42:27 -0300

    fixed bug "Resolver error at paths xxx"

--- src/main/java/com/rysia/conferencedemo/configuration/SwaggerConfiguration.java
+++ src/main/java/com/rysia/conferencedemo/configuration/SwaggerConfiguration.java
@@ -3,21 +3,14 @@ package com.rysia.conferencedemo.configuration;
 import com.rysia.conferencedemo.controllers.HomeController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import static springfox.documentation.builders.PathSelectors.regex;
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -29,29 +22,14 @@ public class SwaggerConfiguration {
     }
 
     @Bean
-    public  Docket api() {
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .paths(regex("/api.*"))
                 .apis(RequestHandlerSelectors.basePackage("com.rysia.conferencedemo"))
                 .build()
                 .useDefaultResponseMessages(false)
-                .globalResponseMessage(RequestMethod.GET, responseMessageGET())
                 .apiInfo(apiInfo());
-    }
-
-    private List<ResponseMessage> responseMessageGET() {
-        return new ArrayList<>() {{
-            add(new ResponseMessageBuilder()
-                    .code(500)
-                    .message("Internal Server Error")
-                    .responseModel(new ModelRef("Error"))
-                    .build());
-            add(new ResponseMessageBuilder()
-                    .code(403)
-                    .message("Forbidden!")
-                    .build());
-        }};
     }
 
     private ApiInfo apiInfo() {
@@ -61,7 +39,8 @@ public class SwaggerConfiguration {
                 .version(homeController.getVersion())
                 .license(homeController.getLicense())
                 .licenseUrl(homeController.getLicenseURL())
-                .contact(new Contact(homeController.getDeveloperName(),
+                .contact(new Contact(
+                        homeController.getDeveloperName(),
                         homeController.getDeveloper(),
                         homeController.getContact()))
                 .build();
